a7ad643cddf2dd756959e060889bd88afffb2cab
Seop0728 <ms_sh0728@naver.com>
2023-02-16 10:30:21 +0900

    👻 FEAT : .env 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BoardModule } from './board/board.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from './config/typeorm.config.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [BoardModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+
+    // forRoot -> 데이터베이스 설정내용을 전역으로 적용
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+    }),
+
+    BoardModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
